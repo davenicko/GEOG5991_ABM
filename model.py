3e6e9a42da710bc314179d6e8b54782541ada738
davenicko <dave_from_leeds@hotmail.com>
2019-01-27 21:53:01 +0000

    Added file outputs

--- model.py
+++ model.py
@@ -3,76 +3,96 @@
 model.py
 
 Created on Mon Jan 14 14:41:17 2019
-Updated on Tue Jan 22
+Updated on Thur Jan 24
 
 @author: David Nicholson
 
 A simple agent based model
 
 Changes:
-    - Added the environment
-    - Added a call to the interaction method
-    - Added code to display the agents and environment
+    - Output of environment to file added
+    - Output of total consumption of agents to a file added
+    - Made the agents use the entire environment (but not properly error
+      checked - yet!)
+
 """
 
 import csv
 import matplotlib.pyplot as plt
 import agentframework
 
+num_of_agents = 100
+num_of_iterations = 10000
+agents = []
+rowlist = []
+environment = []
+rowNum = 0
+colNum = 0
+colNumList = []
+
 def distance_between(agents_row_a, agents_row_b):
     return (((agents_row_a.x - agents_row_b.x)**2) +
         ((agents_row_a.y - agents_row_b.y)**2))**0.5
 
-num_of_agents = 100
-num_of_iterations = 1000
-agents = []
-rowlist = []
-environment = []
+# check all rows have same number of columns
+def colCheck():
+
+    incorrectCols = []
+
+    for col in colNumList:
+        if col == (sum(colNumList) / (len(colNumList))):
+            incorrectCols.append(0)
+        else:
+            incorrectCols.append(1)
+
+    if sum(incorrectCols) != 0:
+        return False
+    else:
+        return True
 
 with open('in.txt', newline='') as file:
     dataset = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
     for row in dataset:
+        rowNum += 1
         for values in row:
             rowlist.append(values)
+            colNum += 1
+        colNumList.append(colNum)
+        colNum = 0
         environment.append(rowlist)
         rowlist=[]
 
-# Make the agents.
-for i in range(num_of_agents):
-    agents.append(agentframework.Agent(environment))
-
-# Move the agents.
-for j in range(num_of_iterations):
+#If the columns are all the same length, make the agents
+#If not all the same length things get a bit more complicated!
+if colCheck():
+    # Make the agents.
     for i in range(num_of_agents):
-        agents[i].move()
-        agents[i].eat()
+        agents.append(agentframework.Agent(environment, rowNum, colNumList[0]))
+
+    # Move the agents.
+    for j in range(num_of_iterations):
+        for i in range(num_of_agents):
+            agents[i].move()
+            agents[i].eat()
 
 
-plt.xlim(0, 99)
-plt.ylim(0, 99)
-plt.imshow(environment)
-for i in range(num_of_agents):
-    plt.scatter(agents[i].x,agents[i].y)
-plt.show()
+    plt.xlim(0, colNumList[0])
+    plt.ylim(0, rowNum)
+    plt.imshow(environment)
+    for i in range(num_of_agents):
+        plt.scatter(agents[i].x,agents[i].y)
+    plt.show()
 
-#for agents_row_a in agents:
-#    for agents_row_b in agents:
-#        distance = distance_between(agents_row_a, agents_row_b)
+    # Output environment to a file
+    with open('environmentOutput.csv', 'w', newline = '') as file2:
+        writer = csv.writer(file2)
+        for row in environment:
+            writer.writerow(row)
 
-plt.imshow(environment)
-plt.show()
+    # Output total units consumed by agents to a file
+    with open('agentOutput.txt', 'a') as file3:
+        total = 0
+        for agent in agents:
+            total += agent.store
+        file3.write(str(total) + "\n")
 
-'''
-#TODO:
-    - Output environment to a file
-    - Output the total amount stored by agents to a file
-        - Option: ppend the data rather than delete
-    - Override __str__(self) to display location when printed
-    - Get agents to wander around the full environment - finding out the size
-      and then using this value to randomize starting locations and delaing
-      with boundaries
-    - Get the agents to eat all the remaining units when <10 without leaving
-      negative numbers
-    - Agents should vomit thier store if the store > 100 units
-
-'''

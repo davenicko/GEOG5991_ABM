a9ede112d63e30f7f8adc436c5ed61a028eb2ace
Dave Nicholson <dave_from_leeds@hotmail.com>
2019-03-11 22:33:55 +0000

    added more commments and made more PEP-8git add model.py

--- model.py
+++ model.py
@@ -23,11 +23,11 @@ import agentframework
 num_of_agents = 100
 num_of_iterations = 10000
 agents = []
-rowlist = []
+row_list = []
 environment = []
-rowNum = 0
-colNum = 0
-colNumList = []
+row_num = 0
+col_num = 0
+col_num_list = []
 
 def distance_between(agents_row_a, agents_row_b):
     '''
@@ -37,24 +37,28 @@ def distance_between(agents_row_a, agents_row_b):
     returns: The euclidian distance between the two agents
     '''
     return (((agents_row_a.x - agents_row_b.x)**2) +
-        ((agents_row_a.y - agents_row_b.y)**2))**0.5
+            ((agents_row_a.y - agents_row_b.y)**2))**0.5
 
 # check all rows have same number of columns
-def colCheck():
+def col_check():
     '''
-    Function to check all rows have the same number of columns
+    Function to check all rows have the same number of columns. I have
+    used this to make sure we can consistently move the agents to the
+    other side of the "world". If this were not in place it is possible
+    that we could get an out of range error. With the data included
+    this is not strictly necessary, but I added it here regardless.
 
     returns:    True if all columns are the same, False otherwise
     '''
-    incorrectCols = []
+    incorrect_cols = []
 
-    for col in colNumList:
-        if col == (sum(colNumList) / (len(colNumList))):
-            incorrectCols.append(0)
+    for col in col_num_list:
+        if col == (sum(col_num_list) / (len(col_num_list))):
+            incorrect_cols.append(0)
         else:
-            incorrectCols.append(1)
+            incorrect_cols.append(1)
 
-    if sum(incorrectCols) != 0:
+    if sum(incorrect_cols) != 0:
         return False
     else:
         return True
@@ -62,21 +66,22 @@ def colCheck():
 with open('in.txt', newline='') as file:
     dataset = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
     for row in dataset:
-        rowNum += 1
+        row_num += 1
         for values in row:
-            rowlist.append(values)
-            colNum += 1
-        colNumList.append(colNum)
-        colNum = 0
-        environment.append(rowlist)
-        rowlist=[]
+            row_list.append(values)
+            col_num += 1
+        col_num_list.append(col_num)
+        col_num = 0
+        environment.append(row_list)
+        row_list = []
 
 #If the columns are all the same length, make the agents
 #If not all the same length things get a bit more complicated!
-if colCheck():
+if col_check():
     # Make the agents.
     for i in range(num_of_agents):
-        agents.append(agentframework.Agent(environment, rowNum, colNumList[0]))
+        agents.append(agentframework.Agent(environment, row_num,
+                                           col_num_list[0]))
 
     # Move the agents.
     for j in range(num_of_iterations):
@@ -85,15 +90,15 @@ if colCheck():
             agents[i].eat()
 
 
-    plt.xlim(0, colNumList[0])
-    plt.ylim(0, rowNum)
+    plt.xlim(0, col_num_list[0])
+    plt.ylim(0, row_num)
     plt.imshow(environment)
     for i in range(num_of_agents):
-        plt.scatter(agents[i].x,agents[i].y)
+        plt.scatter(agents[i].x, agents[i].y)
     plt.show()
 
     # Output environment to a file
-    with open('environmentOutput.csv', 'w', newline = '') as file2:
+    with open('environmentOutput.csv', 'w', newline='') as file2:
         writer = csv.writer(file2)
         for row in environment:
             writer.writerow(row)
